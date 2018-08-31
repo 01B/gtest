7774146545975d242947cb3d3d12880d747f8208
01b <no.pain.no.code@gmail.com>
2018-08-31 12:47:13 +0900

    @Transactional annotation 수정

--- src/main/java/com/nopainnocode/gamehubtest/user/service/UserService.java
+++ src/main/java/com/nopainnocode/gamehubtest/user/service/UserService.java
@@ -8,14 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-
 /**
  * Created by no.pain.no.code@gmail.com
  * nopainnocode.tistory.com
  */
 @Service
-@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
@@ -40,10 +37,11 @@ public class UserService {
      * 저장된 회원들과 저장하려고 하는 회원의 이름 중복 검사
      */
     private void ifUserNameDuplicatedThrowException(User user) {
-        for (User userTemp : userRepository.findAll()) {
-            if (userTemp.isNameDuplicated(user)) {
-                throw new InvalidNameException();
-            }
+        if (
+                userRepository.findByFirstNameAndMiddleName(user.getFirstName(), user.getMiddleName()).size() > 0
+                || userRepository.findByFirstNameAndLastName(user.getFirstName(), user.getLastName()).size() > 0
+                || userRepository.findByMiddleNameAndLastName(user.getMiddleName(), user.getLastName()).size() > 0) {
+            throw new InvalidNameException();
         }
     }
 }
